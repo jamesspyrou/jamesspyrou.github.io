03d235186ca923a4ddbaea9513be651f712a49ae
jamesspyrou <jamesspyrou@gmail.com>
2025-09-24 14:17:53 +1000

    Update loadsections.js

--- sections/loadsections.js
+++ sections/loadsections.js
@@ -1,28 +1,40 @@
- var sections = ["images", "bio", "publications", "awards", "presentations"];
-  function loadSection(section) {
+var sections = ["images", "bio", "publications", "awards", "presentations"];
+function loadSection(section) {
     var xhr = null;
     if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
+        xhr = new XMLHttpRequest();
     } else {
-      try {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-      } catch (e) {
-        console.error("AJAX not supported in this browser.");
-        return;
-      }
+        try {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            alert("AJAX not supported in this browser.");
+            return;
+        }
     }
     xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          document.getElementById(section).innerHTML = xhr.responseText;
-        } else {
-          document.getElementById(section).innerHTML = "Error loading " + section;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 0) {
+                var el = document.getElementById(section);
+                if (el) el.innerHTML = xhr.responseText;
+            } else {
+                var el = document.getElementById(section);
+                if (el) el.innerHTML = "Error loading " + section;
+            }
         }
-      }
     };
     xhr.open("GET", "sections/" + section + ".html", true);
     xhr.send();
-  }
-  for (var i = 0; i < sections.length; i++) {
-    loadSection(sections[i]);
-  }
+}
+if (window.attachEvent) { // IE7/8
+    window.attachEvent('onload', function() {
+        for (var i = 0; i < sections.length; i++) {
+            loadSection(sections[i]);
+        }
+    });
+} else {
+    window.onload = function() {
+        for (var i = 0; i < sections.length; i++) {
+            loadSection(sections[i]);
+        }
+    };
+}
