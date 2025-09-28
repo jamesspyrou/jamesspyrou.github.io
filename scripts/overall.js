e2c19a882a71bfd27da5680926c70d5fc086879b
jamesspyrou <jamesspyrou@gmail.com>
2025-09-28 18:07:38 +1000

    Changed to single-section with buttons

--- scripts/overall.js
+++ scripts/overall.js
@@ -1,82 +1,48 @@
-var sections = ["navigation", "images", "bio", "contact", "publications", "awards", "presentations"];
-function loadSection(section) {
-    var xhr = null;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        try {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        } catch (e) {
-            alert("AJAX not supported in this browser.");
-            return;
+function loadSection(section, btn) {
+      var container = document.getElementById("main-container");
+      var xhr = new XMLHttpRequest();
+
+      xhr.open("GET", "./sections/" + section + ".html", true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          container.classList.remove("show"); 
+          container.innerHTML = xhr.responseText;
+
+          var buttons = document.querySelectorAll('.nav-btn');
+          for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('active');
+          }
+
+          if (btn) btn.classList.add('active');
+
+          if (location.hash !== "#" + section) {
+            history.replaceState(null, "", "#" + section);
+          }
+
+          setTimeout(function() {
+            container.classList.add("show");
+          }, 10);
         }
+      };
+      xhr.send();
     }
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200 || xhr.status === 0) {
-                var el = document.getElementById(section);
-                if (el) el.innerHTML = xhr.responseText;
-            } else {
-                var el = document.getElementById(section);
-                if (el) el.innerHTML = "Error loading " + section;
-            }
-        }
-    };
-    xhr.open("GET", "sections/" + section + ".html", true);
-    xhr.send();
-}
-if (window.attachEvent) {
-    window.attachEvent('onload', function() {
-        for (var i = 0; i < sections.length; i++) {
-            loadSection(sections[i]);
-        }
+
+    function initDefaultSection(defaultSection) {
+      if (!defaultSection) defaultSection = "bio";
+      var sectionFromHash = window.location.hash.replace("#", "") || defaultSection;
+
+      var targetBtn = document.querySelector('.nav-btn[onclick*="' + sectionFromHash + '"]');
+      loadSection(sectionFromHash, targetBtn);
+    }
+
+    window.addEventListener("hashchange", function() {
+      var section = window.location.hash.replace("#", "");
+      if (section) {
+        var targetBtn = document.querySelector('.nav-btn[onclick*="' + section + '"]');
+        loadSection(section, targetBtn);
+      }
     });
-} else {
+
     window.onload = function() {
-        for (var i = 0; i < sections.length; i++) {
-            loadSection(sections[i]);
-        }
+      initDefaultSection("bio");
     };
-}
-
-const toc = document.querySelector('.toc');
-const titlebar = toc.querySelector('.titlebar');
-console.log(toc, titlebar);
-
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
-
-titlebar.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  isDragging = true;
-  offsetX = e.clientX - toc.offsetLeft;
-  offsetY = e.clientY - toc.offsetTop;
-});
-
-document.addEventListener('mousemove', (e) => {
-  if (isDragging) {
-    toc.style.left = (e.clientX - offsetX) + "px";
-    toc.style.top = (e.clientY - offsetY) + "px";
-  }
-});
-
-document.addEventListener('mouseup', () => {
-  isDragging = false;
-});
-
-window.addEventListener('load', function() {
-    const nav = document.querySelector('.toc');
-    const form = document.querySelector('#form_container');
-    
-    const navRect = nav.getBoundingClientRect();
-    const formRect = form.getBoundingClientRect();
-
-	const buffer = 4; 
-
-    if (navRect.right > formRect.left + buffer) {
-        nav.style.visibility = 'hidden';
-    } else {
-        nav.style.visibility = 'visible';
-    }
-});
