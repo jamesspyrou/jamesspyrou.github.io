0811e7df6d30d886865a4766a67072257a4d822f
jamesspyrou <jamesspyrou@gmail.com>
2025-10-02 09:57:57 +1000

    Add citations.js

--- scripts/overall.js
+++ scripts/overall.js
@@ -46,16 +46,3 @@ function loadSection(section, btn) {
     window.onload = function() {
       initDefaultSection("bio");
     };
- 
-const authorId = "113826046";  
-
-fetch(`https://api.semanticscholar.org/graph/v1/author/${authorId}?fields=citationCount`)
-	.then(response => response.json())
-	.then(data => {
-		document.getElementById("citations").textContent = data.citationCount.toLocaleString();
-    })
-	.catch(error => {
-		console.error("Error fetching citation count:", error);
-	document.getElementById("citations").textContent = "N/A";
-	});
-	
